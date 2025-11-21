177661ff95a99b9c39377f6860f3b44817749f60
Edder Flores <floresmoy11@gmail.com>
2025-11-21 16:16:45 -0600

    updated activyt 1 to original activity, and updated 2nd project to the grade program from week 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.WriteLine("Hi there! Welcome to the grade program!");
+        Console.Write("Please enter your grade percentage (0-100): ");
+        float gradePercentage = float.Parse(Console.ReadLine());
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter != "F")
+        {
+            Console.WriteLine($"Your letter grade is: {letter}, you passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Your letter grade is: {letter}, you failed. Try better next time!");
+    }
     }
 }
